b8fa013ef369e8606733c93f4ddd5f7582a99ec4
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2023-10-07 14:23:47 -0700

    FRNT: Implement basic table UI

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TableModule } from 'primeng/table';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
