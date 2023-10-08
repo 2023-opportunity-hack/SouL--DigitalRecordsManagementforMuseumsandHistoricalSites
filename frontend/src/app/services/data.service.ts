b977ce44a4427d84a461b171f6b610fd8cc99db6
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2023-10-08 12:15:46 -0700

    FRNT: trivial change

--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from  '@angular/common/http';
   providedIn: 'root'
 })
 export class DataService {
-  url = 'https://localhost:8080/'
+  url = 'http://localhost:8080/'
 
   constructor(private http: HttpClient) { }
 
