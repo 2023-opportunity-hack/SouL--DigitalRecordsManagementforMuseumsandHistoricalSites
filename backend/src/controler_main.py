6025d25054c1970c040f84f2afafba83495ecb07
TranCongThinh <congthinh6304@gmail.com>
2023-10-07 23:21:56 -0700

    delete folder

--- backend/src/controler_main.py
+++ backend/src/controler_main.py
@@ -1,7 +1,6 @@
 from pptx2pdf.pptx_converter import pptx2text
 from image_captioning_module.image_caption import caption
 from text_file_utility.txt_reader import read_file
-import sys
 
 
 def preprocess_input(file_path):
