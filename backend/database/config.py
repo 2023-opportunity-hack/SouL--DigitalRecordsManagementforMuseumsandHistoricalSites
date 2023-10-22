9e0f3f320583a707b5ff5c49014116581f046d2b
Amey <bamey2241997@gmail.com>
2023-10-21 19:43:55 -0700

    Added Changes to config.oy

--- backend/database/config.py
+++ backend/database/config.py
@@ -3,8 +3,7 @@ from runnable_configs import ENVIRONMENT
 
 
 KEY_PATH = "../../fernetKey.key"
-USER_NAME = "test"
-PASSWORD = b'gAAAAABlIg0vQSh48oNMLQf8L0cSKXBUUfmvKJY75WKCEMJn7jP6WR9YtR0xhJk7SUWfOPGRz8vCY0OSbYgt9mmXR6lYGJ9e1g=='
+
 
 # ELASTIC SEARCH CLASS CONFIGS
 HOST = "localhost"
@@ -14,6 +13,10 @@ URL = f"http://{HOST}:{PORT}"
 # CONFIGS DEPENDEDNT ON ENVRONMENT
 if ENVIRONMENT.lower() == "dev":
     INDEX_NAME = "search_index_dev"
+    USER_NAME = "test"
+    PASSWORD = b'gAAAAABlIg0vQSh48oNMLQf8L0cSKXBUUfmvKJY75WKCEMJn7jP6WR9YtR0xhJk7SUWfOPGRz8vCY0OSbYgt9mmXR6lYGJ9e1g=='
 else:
     INDEX_NAME = "search_index_prod"
+    USER_NAME = "prod"
+    PASSWORD = b'gAAAAABlIg0vQSh48oNMLQf8L0cSKXBUUfmvKJY75WKCEMJn7jP6WR9YtR0xhJk7SUWfOPGRz8vCY0OSbYgt9mmXR6lYGJ9e1g=='
 
